16e13824c80d697cef63d157b492de61eeee7bcc
tduthil <thomas.duthil@etu.u-bordeaux.fr>
2022-03-28 21:30:34 +0200

    BUG SIMON

--- Simon/script.js
+++ Simon/script.js
@@ -1,25 +1,82 @@
-$(document).ready(function(){
-    $(".pad").css("opacity","0.5");
-    $(".pad").on("click",function(){
-        $(this).css("opacity","1");
-    });
-    $(".pad").on("mouseover",function(){
-        $(this).css("opacity","0.7");
-    });
-    $(".pad").on("mouseout",function(){
-        $(this).css("opacity","0.5");
-    });
-    $(".shape1").on("click",function(){
-        new Audio("sounds_01.mp3").play();
-    });
-    $(".shape2").on("click",function(){
-        new Audio("sounds_02.mp3").play();
-    });
-    $(".shape3").on("click",function(){
-        new Audio("sounds_03.mp3").play();
-    });
-    $(".shape4").on("click",function(){
-        new Audio("sounds_04.mp3").play();
-    });
+var level = 0;
+var sequence = [];
+var usedsequence = [];
+var playing = false;
+var animation = false;
+
+function startGame() {
+    if(!animation && !playing){
+        animation = true;
+        $(".level").text("level: " + (++level));
+        RandomSequence()
+        playSequence();
+    }
     
+}
+
+function randNum(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+  }
+
+function RandomSequence() {
+    let allPads = $(".pad")
+    let pad = allPads[Math.floor(Math.random() * allPads.length)]
+    sequence.push(pad);
+}
+
+
+function playSequence() {
+    for (var i = 0; i < sequence.length; i++) {
+        var delayTime = i * 500;
+        setTimeout(AfficherSequence, delayTime);
+    }
+    animation = false;
+    playing = true;
+}
+
+function AfficherSequence() {
+    let pad = sequence.pop();
+    $(pad).children()[0].play();
+    $(pad).fadeTo(400, 0.5, function () {
+        $(this).fadeTo(400, 1, AfficherSequence);
+    });
+    usedsequence.push(pad);
+}
+
+function click(){
+    if(playing){
+        var lost = false
+        var item = usedsequence.shift();
+        $(this).children()[0].play();
+        $(this).fadeTo(400, 0.5, function () {
+            let lenght = usedsequence.length
+            function checkLost() {
+                if(lenght == 0 && !lost){
+                    playing = false;
+                    startGame();
+                    console.log("On recommence ")
+                }
+            }
+            $(this).fadeTo(400, 1, checkLost);
+        });
+
+        
+        var itemvalue = $(item).attr("class").split("shape")[1]
+        var played = $(this).attr("class").split("shape")[1];
+        if (itemvalue != played) {
+            lost = true;
+            sequence = []
+            level = 0;
+            playing = false;
+            $(".level").text("Vous avez perdu :(")
+        }
+        sequence.push(item);
+    }
+    
+}
+
+$(document).ready(function () {
+    
+    $(".circle").on("click", startGame);
+    $(".pad").on("click", click);
 });
